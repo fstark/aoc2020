6744a8d2bf09e85d0aff33d0c1a307f5756d8f29
Fred Stark <fstark@kds.com>
2020-12-03 12:34:56 +0100

    03 b solved

--- main.cpp
+++ main.cpp
@@ -3,15 +3,27 @@
 #include <vector>
 #include <string>
 
-using namespace std;
+const int W = 31;
+const int H = 323;
+
+char data[W][H];
+
+int count_trees( int w, int h )
+{
+    int total = 0;
+    int i=0;
+    int x=0;
+    for (int i=0;i<H;i+=h)
+    {
+        total += data[x][i]=='#';
+        x += w;
+        x %= W;
+    }
+    return total;
+}
 
 int main()
 {
-    const int W = 31;
-    const int H = 323;
-
-    char data[W][H];
-
     for (int y=0;y!=H;y++)
         for (int x=0;x!=W;x++)
         {
@@ -23,11 +35,7 @@ int main()
             }   while (c!='.' && c!='#');
         }
 
-    int total = 0;
-    for (int i=0;i!=H;i++)
-        total += data[(3*i)%W][i]=='#';
-
-    printf( "%d\n", total );
+    printf( "%d\n", count_trees(1,1)*count_trees(3,1)*count_trees(5,1)*count_trees(7,1)*count_trees(1,2) );
 
     return EXIT_SUCCESS;
 }
