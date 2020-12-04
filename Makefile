c227cbf2c6dfbe892e21aa3a6ed42713d58f9cc7
Fred Stark <fstark@kds.com>
2020-12-04 11:55:25 +0100

    04 a solved

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
+test: go
+	(cat input.txt ; echo ; echo "END" ) | tr '\012' '#' | sed -e 's/##/@/g' | sed -e 's/#/ /g' | sed -e 's/  / /g' | sed -e 's/@/ DONE@/g' | tr '@' '\012' | tr ':' ' ' | ./go
+
 go: main.cpp
 	g++ -std=c++17 main.cpp -lc++ -o go
-	./go < input.txt
 
 clean:
 	@rm -f go
--- main.cpp
+++ main.cpp
@@ -2,40 +2,51 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
-const int W = 31;
-const int H = 323;
-
-char data[W][H];
-
-int count_trees( int w, int h )
-{
-    int total = 0;
-    int i=0;
-    int x=0;
-    for (int i=0;i<H;i+=h)
-    {
-        total += data[x][i]=='#';
-        x += w;
-        x %= W;
-    }
-    return total;
-}
+#include <map>
 
 int main()
 {
-    for (int y=0;y!=H;y++)
-        for (int x=0;x!=W;x++)
+    std::string k;
+    std::string v;
+    std::map<std::string,std::string> m;
+    int valid = 0;
+    int total = 0;
+
+    while (!std::cin.eof())
+    {
+        std::cin >> k;
+        std::cout << "K" << k << " ";
+        if (k=="DONE")
         {
-            int c;
-            do
-            {
-                c = getchar();
-                data[x][y] = c;
-            }   while (c!='.' && c!='#');
+            if (m.count("byr")
+            && m.count("iyr")
+            && m.count("eyr")
+            && m.count("hgt")
+            && m.count("hcl")
+            && m.count("ecl")
+            && m.count("pid")
+            )
+                valid++;
+            m.clear();
+            total++;
+            continue;
+        }
+        if (k=="END")
+        {
+            printf( "%d\n", valid );
+            printf( "%d\n", total );
+            break;
         }
 
-    printf( "%d\n", count_trees(1,1)*count_trees(3,1)*count_trees(5,1)*count_trees(7,1)*count_trees(1,2) );
+        std::cin >> v;
+        m[k] = v;
+
+        std::cout << v << " ";
+        // if (v=="DONE")
+        //     break;
+    }
+
+    std::cout << "EOF\n";
 
     return EXIT_SUCCESS;
 }
