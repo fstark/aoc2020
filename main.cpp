afdf784e6e78e7e67843f7c955bd543da5373c37
Fred Stark <fstark@kds.com>
2020-12-07 10:29:11 +0100

    07 a solved

--- main.cpp
+++ main.cpp
@@ -3,42 +3,97 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <set>
 #include <cassert>
 using namespace std::string_literals;
 
+std::string scan_color()
+{
+    std::string c1,c2;
+    std::cin >> c1;
+    std::cin >> c2;
+    return c1+"-"+c2;
+}
+
+void scan_contain()
+{
+    std::string contains;
+    std::cin >> contains;
+    // std::cout << "[" << contains << "]";
+    assert( contains=="contain" );
+}
+
+int scan_number()
+{
+    std::string ns;
+    std::cin >> ns;
+    if (ns=="no")
+    {
+        std::cin >> ns;
+        return 0;
+    }
+    return atoi(ns.c_str());
+}
+
+bool scan_end()
+{
+    std::string bag;
+    std::cin >> bag;
+    return bag[bag.size()-1]=='.';
+}
+
 int main()
 {
-    std::string s;
+    std::multimap<std::string,std::string> reverse_map;
 
-    int seen[26];
-    for (int i=0;i!=26;i++)
-        seen[i] = 0;
-
-    int total = 0;
-    int count = 0;
-
-    while (std::getline(std::cin,s))
+    while (!std::cin.eof())
     {
-        // std::cout << "[" << s << "]";
-        if (s=="")
+        auto c = scan_color();
+        if (std::cin.eof())
+            break;
+        scan_end();
+        scan_contain();
+        std::cout << c << ": ";
+        do
         {
-            for (auto b:seen)
-                total+=b==count;
-            for (int i=0;i!=26;i++)
-                seen[i] = 0;
-            count = 0;
+            int n = scan_number();
+            if (n)
+            {
+                auto b = scan_color();
+                std::cout << "[" << b << "]";
+                reverse_map.insert({b,c});
+            }
+        }   while (!scan_end() && !std::cin.eof());
+        std::cout << "\n";
+    }
+
+    std::set<std::string> bags;
+    bags.insert( "shiny-gold"s );
+
+    while (true)
+    {
+        std::set<std::string> more_bags;
+        for (auto &c:bags)
+        {
+            more_bags.insert( c );
+            auto i0 = reverse_map.lower_bound(c); 
+            auto i1 = reverse_map.upper_bound(c);          
+            while (i0!=i1)     
+            { 
+                if (i0->first==c) 
+                    more_bags.insert( i0->second );         
+                i0++; 
+            }     
+        }
+        if (more_bags.size()>bags.size())
+        {
+            bags = more_bags;
         }
         else
-        {
-            for (auto c:s)
-                seen[c-'a']++;
-            count++;
-        }
+            break;
     }
-    for (auto b:seen)
-        total+=b==count;
 
-    std::cout << total << "\n";
+    std::cout << bags.size()-1 << "\n";
 
     return EXIT_SUCCESS;
 }
