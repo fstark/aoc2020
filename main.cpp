a9023e64931a6d17b5bfddf5c4bb50657d37c06b
Fred Stark <fstark@kds.com>
2020-12-07 10:49:49 +0100

    07 b solved

--- main.cpp
+++ main.cpp
@@ -42,10 +42,33 @@ bool scan_end()
     return bag[bag.size()-1]=='.';
 }
 
+struct bags
+{
+    int count;
+    std::string color;
+};
+
+std::multimap<std::string,bags> map;
+
+int count_bags( const std::string &color )
+{
+    // std::cout << "{" << color << "}" << std::flush;
+
+    int total = 1;
+    auto it = map.find(color);
+    if (it==std::end(map))
+        return total;
+    while (it->first==color)
+    {
+        // std::cout << "<" << color << "/" << it->first << ":" << it->second.count << "*" << it->second.color << ">" << std::flush;
+        total += it->second.count*count_bags( it->second.color );
+        it++;
+    }
+    return total;
+}
+
 int main()
 {
-    std::multimap<std::string,std::string> reverse_map;
-
     while (!std::cin.eof())
     {
         auto c = scan_color();
@@ -53,47 +76,21 @@ int main()
             break;
         scan_end();
         scan_contain();
-        std::cout << c << ": ";
+        // std::cout << c << ": ";
         do
         {
             int n = scan_number();
             if (n)
             {
                 auto b = scan_color();
-                std::cout << "[" << b << "]";
-                reverse_map.insert({b,c});
+                // std::cout << "[" << b << "]";
+                map.insert({c,{n,b}});
             }
         }   while (!scan_end() && !std::cin.eof());
-        std::cout << "\n";
+        // std::cout << "\n";
     }
 
-    std::set<std::string> bags;
-    bags.insert( "shiny-gold"s );
-
-    while (true)
-    {
-        std::set<std::string> more_bags;
-        for (auto &c:bags)
-        {
-            more_bags.insert( c );
-            auto i0 = reverse_map.lower_bound(c); 
-            auto i1 = reverse_map.upper_bound(c);          
-            while (i0!=i1)     
-            { 
-                if (i0->first==c) 
-                    more_bags.insert( i0->second );         
-                i0++; 
-            }     
-        }
-        if (more_bags.size()>bags.size())
-        {
-            bags = more_bags;
-        }
-        else
-            break;
-    }
-
-    std::cout << bags.size()-1 << "\n";
+    std::cout << count_bags( "shiny-gold" )-1 << "\n";
 
     return EXIT_SUCCESS;
 }
