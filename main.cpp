907d8a5d4688b36ce6ffa2ad0b2654ccf1d2033f
Fred Stark <fstark@kds.com>
2020-12-09 12:39:03 +0100

    08 b solved

--- main.cpp
+++ main.cpp
@@ -13,6 +13,44 @@ struct instr
     int value;
 };
 
+int exec( std::vector<instr> prog, int i )
+{
+    if (prog[i].code=="nop")
+        prog[i].code = "jmp";
+    else
+        if (prog[i].code=="jmp")
+            prog[i].code = "nop";
+
+    int pc = 0;
+    int a = 0;
+    int count = 0;
+    while (count++ <= prog.size())
+    {
+        // std::cout << pc << "," << a << ":" << prog[pc].code << " " << prog[pc].value << "\n";
+        auto i = prog[pc].code;
+        if (i=="acc")
+        {
+            a = a + prog[pc].value;
+            pc++;
+        }
+        else if (i=="nop")
+        {
+            pc++;
+        }
+        else if (i=="jmp")
+        {
+            pc += prog[pc].value;
+        }
+
+        if (pc<0 || pc>=prog.size())
+            return a;
+    }
+
+    // std::cout << "\n";
+
+    return 0; // ?ugh
+}
+
 int main()
 {
     std::vector<instr> prog;
@@ -30,29 +68,14 @@ int main()
         prog.push_back( { instr, value } );
     }
 
-    int pc = 0;
     int a;
-    while (prog[pc].code!="")
+    for (int i=0;i!=prog.size();i++)
     {
-        // std::cout << pc << "," << a << ":" << prog[pc].code << " " << prog[pc].value << "\n";
-        auto i = prog[pc].code;
-        prog[pc].code = "";
-         if (i=="acc")
-        {
-            a = a + prog[pc].value;
-            pc++;
-        }
-        else if (i=="nop")
-        {
-            pc++;
-        }
-        else if (i=="jmp")
-        {
-            pc += prog[pc].value;
-        }
-
+        // std::cout << i << "\n";
+        a = exec( prog, i );
+        if (a)
+            break;
     }
-
     std::cout << a << "\n";
 
     return EXIT_SUCCESS;
