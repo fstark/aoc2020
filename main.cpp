c720b2a942175e35c0aefe675fe19c13514ddfbb
Fred Stark <fstark@kds.com>
2020-12-10 12:15:16 +0100

    10 b solved

--- main.cpp
+++ main.cpp
@@ -8,26 +8,47 @@
 #include <cassert>
 using namespace std::string_literals;
 
+static std::vector<long long> memo;
+
+template <typename IT> long long count_possibilities( int current, IT b, IT e )
+{
+    int n = e-b;
+    while (memo.size()<=n)
+        memo.push_back( 0 );
+    if (memo[n]!=0)
+        return memo[n];
+
+    if (b==e)
+        return 1;
+    long long total = 0;
+    
+    while (b!=e)
+    {
+        if (*b<=current+3)
+            total += count_possibilities( *b, b+1, e );
+        else
+            break;
+        b++;
+    }
+
+    memo[n] = total;
+
+    return total;
+}
+
 int main()
 {
-    int current = 0;
-    int n1 = 0;
-    int n3 = 1;
+    std::vector<int> data;
 
     while (!std::cin.eof())
     {
-        long long n;
+        int long n;
         std::cin >> n;
-        int diff = n-current;
-        if (diff==1)
-            n1++;
-        if (diff==3)
-            n3++;
-        // std::cout << current << " + " << n-current << " => " << n << "\n";
-        current = n;
+        if (!std::cin.eof())
+            data.push_back( n );
     }
 
-    std::cout << n1 * n3 << "\n";
+    std::cout << count_possibilities( 0, std::begin(data), std::end(data) ) << "\n";
 
     return EXIT_SUCCESS;
 }
