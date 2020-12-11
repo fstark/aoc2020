65cbbab5284ba22fe66009fd78f7caa0b9f3b4b2
Fred Stark <fstark@kds.com>
2020-12-11 16:07:09 +0100

    11 a solved

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test: go
-	sort -n input.txt | ./go
+	cat input.txt | ./go
 
 go: main.cpp
 	g++ -std=c++17 main.cpp -lc++ -o go
--- main.cpp
+++ main.cpp
@@ -8,47 +8,90 @@
 #include <cassert>
 using namespace std::string_literals;
 
-static std::vector<long long> memo;
+const int W = 95;
+const int H = 99;
+// const int W = 10;
+// const int H = 10;
 
-template <typename IT> long long count_possibilities( int current, IT b, IT e )
+std::array<std::array<int,W>,H> room;
+std::array<std::array<int,W>,H> room2;
+
+int count_seats( int x0, int y0 )
 {
-    int n = e-b;
-    while (memo.size()<=n)
-        memo.push_back( 0 );
-    if (memo[n]!=0)
-        return memo[n];
-
-    if (b==e)
-        return 1;
-    long long total = 0;
-    
-    while (b!=e)
-    {
-        if (*b<=current+3)
-            total += count_possibilities( *b, b+1, e );
-        else
-            break;
-        b++;
-    }
-
-    memo[n] = total;
-
-    return total;
+    int count = 0;
+    for (int dx=-1;dx!=2;dx++)
+        for (int dy=-1;dy!=2;dy++)
+            if (dx!=0 || dy!=0)
+            {
+                int x = x0+dx;
+                int y = y0+dy;
+                if (x>=0 && x<W && y>=0 && y<H)
+                    count += room[y][x]==1;
+            }
+    return count;
 }
 
 int main()
 {
-    std::vector<int> data;
 
-    while (!std::cin.eof())
+    for (int y=0;y!=H;y++)
+        for (int x=0;x!=W;x++)
+        {
+            while (true)
+            {
+                int c = getchar();
+                if (c=='L')
+                    room[y][x] = 0;    // free
+                else if (c=='.')
+                    room[y][x] = -1;   //  floor
+                else
+                    continue;
+                break;
+            }
+        }
+
+    for (int y=0;y!=H;y++)
     {
-        int long n;
-        std::cin >> n;
-        if (!std::cin.eof())
-            data.push_back( n );
+        for (int x=0;x!=W;x++)
+            std::cout << (room[y][x]==1?'#':(room[y][x]==-1?'.':'L'));
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+
+
+    while (true)
+    {
+        for (int y=0;y!=H;y++)
+            for (int x=0;x!=W;x++)
+            {   
+                int count = count_seats( x, y );
+                room2[y][x] = room[y][x];
+                if (room[y][x]==0)
+                    room2[y][x] = count==0?1:0;
+                if (room[y][x]==1)
+                    room2[y][x] = count>=4?0:1;
+            }
+        if (room==room2)
+            break;
+        room = room2;
+
+        for (int y=0;y!=H;y++)
+        {
+            for (int x=0;x!=W;x++)
+                std::cout << (room[y][x]==1?'#':(room[y][x]==-1?'.':'L'));
+            std::cout << "\n";
+        }
+        std::cout << "\n";
+
+
     }
 
-    std::cout << count_possibilities( 0, std::begin(data), std::end(data) ) << "\n";
+    int total = 0;
+    for (int y=0;y!=H;y++)
+        for (int x=0;x!=W;x++)
+            total +=room[y][x]==1;
+
+    std::cout << total << "\n";
 
     return EXIT_SUCCESS;
 }
