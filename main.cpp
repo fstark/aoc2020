42d4020d25580c4801e163daed23d28ed690ea7e
Fred Stark <fstark@kds.com>
2020-12-12 15:45:26 +0100

    12 b solved

--- main.cpp
+++ main.cpp
@@ -6,20 +6,19 @@ using namespace std::complex_literals;
 
 int main()
 {
-    static const char *direction_names = "ESWN";
-    static const std::complex<int> directions[] = { 1, -1i, -1, 1i };
-    int d = 0;
-    std::complex<int> p;
+    static const char *direction_names = "ENWS";
+    std::complex<double> p;
+    std::complex<double> w = 10.+1i;
     for (;;)
     {
         char c;
-        int n;
+        double n;
         std::cin >> c >> n;
         if (std::cin.eof())
             break;
-        if (c=='F') p += directions[d]*n;
-        else if (c=='R' || c=='L') d = (d+((c=='R')-(c=='L'))*n/90+4)%4;
-        else p += directions[::strchr( direction_names, c )-direction_names]*n;
+        if (c=='F') p += w*n;
+        else if (c=='R' || c=='L') w *= std::pow( ((c=='L')-(c=='R'))*1.*1i, n/90 );
+        else w += std::pow( 1i, (::strchr( direction_names, c )-direction_names) )*n;
     }
 
     std::cout << ::fabs( std::real(p) ) + ::fabs( std::imag(p) ) << std::endl;
