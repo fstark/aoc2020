8eba0ddd8fcc23d7d9016afe8771921fdf1d4cfa
Fred Stark <fstark@kds.com>
2020-12-17 19:46:57 +0100

    17 b solved

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    const int SIZE = 101;
+    const int SIZE = 19;
     const int MIN = -SIZE/2;
     const int MAX = (SIZE+1)/2;
     const int ISIZE = 8;
@@ -15,7 +15,7 @@ int main()
 
     const int MID = SIZE/2;
 
-    static std::array<std::array<std::array<bool,SIZE>,SIZE>,SIZE> cells;
+    static std::array<std::array<std::array<std::array<bool,SIZE>,SIZE>,SIZE>,SIZE> cells;
 
     char c;
 
@@ -23,7 +23,7 @@ int main()
         for (int y=0;y!=ISIZE;y++)
         {
             std::cin >> c;
-            cells[y+MID+IMIN][x+MID+IMIN][MID] = c=='#';
+            cells[y+MID+IMIN][x+MID+IMIN][MID][MID] = c=='#';
             // std::cout << x+MID+IMIN << " " << y+MID+IMIN << " " << MID << " = " << cells[x+MID+IMIN][y+MID+IMIN][MID] << "\n";
         }
 
@@ -47,18 +47,20 @@ int main()
         for (int x=1;x!=SIZE-1;x++)
             for (int y=1;y!=SIZE-1;y++)
                 for (int z=1;z!=SIZE-1;z++)
+                for (int w=1;w!=SIZE-1;w++)
                 {
                     int count = 0;
                     for (int dx=-1;dx!=2;dx++)
                         for (int dy=-1;dy!=2;dy++)
                             for (int dz=-1;dz!=2;dz++)
-                                if (dx || dy || dz)
-                                    count += cells[x+dx][y+dy][z+dz];
+                            for (int dw=-1;dw!=2;dw++)
+                                if (dx || dy || dz || dw)
+                                    count += cells[x+dx][y+dy][z+dz][w+dw];
                     // std::cout << "@"<<x<<y<<z<<":"<<count<<" ";
-                    if (cells[x][y][z])
-                        next[x][y][z] = count==2 || count==3;
+                    if (cells[x][y][z][w])
+                        next[x][y][z][w] = count==2 || count==3;
                     else
-                        next[x][y][z] = count==3;
+                        next[x][y][z][w] = count==3;
                 }
 
         if (next==cells)
@@ -71,7 +73,8 @@ int main()
     for (int x=0;x!=SIZE;x++)
         for (int y=0;y!=SIZE;y++)
             for (int z=0;z!=SIZE;z++)
-                count += cells[x][y][z];
+            for (int w=0;w!=SIZE;w++)
+                count += cells[x][y][z][w];
 
     std::cout << count << "\n";
 
